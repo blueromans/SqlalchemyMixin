7ce14c40e405668321aecf96c70484def00a0b48
Yaşar Özyurt <blueromans@hotmail.com>
2022-10-29 19:28:24 +0300

    fix:Code fix.

--- sqlalchemy_mixins/active_record.py
+++ sqlalchemy_mixins/active_record.py
@@ -1,5 +1,4 @@
 import sqlalchemy as sa
-from slugify import slugify
 
 from .crud_mixin import CrudMixin
 
@@ -8,11 +7,6 @@ class ActiveRecordMixin(CrudMixin):
     __abstract__ = True
     id = sa.Column(sa.Integer, primary_key=True, index=True)
     active = sa.Column(sa.Boolean, nullable=False, server_default='1')
-
-    def __init__(self, *args, **kwargs):
-        if 'slug' in self.settable_attributes:
-            kwargs['slug'] = slugify(kwargs.get('name', ''))
-        super().__init__(*args, **kwargs)
 
     def toggle_status(self):
         self.active = not self.active
