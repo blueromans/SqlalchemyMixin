4cfe1264086c71f6f623ce7671ea18d7f91c870d
Yaşar Özyurt <blueromans@hotmail.com>
2022-11-17 16:40:03 +0300

    fix:Datemixin fix for is_new property.

--- sqlalchemy_mixins/date_mixin.py
+++ sqlalchemy_mixins/date_mixin.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 
 import arrow
@@ -23,6 +24,14 @@ class DateMixin:
                            default=__datetime_func__,
                            onupdate=__datetime_func__,
                            nullable=False)
+
+    @property
+    def is_new(self):
+        if self.created_at is not None:
+            timedelta = round(
+                (datetime.now() - self.created_at).total_seconds() / 60)
+            return timedelta <= os.environ.get('IS_NEW_TIME_INTERVAL', 120)
+        return False
 
     @property
     def create_date(self, tzinfo='Europe/Istanbul', locale='tr_tr', format='DD MMMM YYYY dddd HH:mm'):
