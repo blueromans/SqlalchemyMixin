e763566783a4960067e420dd7b49736ded4f8763
Yaşar Özyurt <blueromans@hotmail.com>
2022-11-22 00:35:28 +0300

    fix:Minor bug fixes about datemixin.

--- sqlalchemy_mixins/date_mixin.py
+++ sqlalchemy_mixins/date_mixin.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 
 import arrow
@@ -25,15 +24,6 @@ class DateMixin:
                            onupdate=__datetime_func__,
                            nullable=False)
 
-    @property
-    def is_new(self):
-        if self.created_at is not None:
-            timedelta = round(
-                (datetime.now() - self.created_at).total_seconds() / 60)
-            return timedelta <= os.environ.get('IS_NEW_TIME_INTERVAL', 120)
-        return False
-
-    @property
     def create_date(self, tzinfo='Europe/Istanbul', locale='tr_tr', format='DD MMMM YYYY dddd HH:mm'):
         if self.created_at is not None:
             timedelta = round((datetime.now() - self.created_at).days)
@@ -42,7 +32,6 @@ class DateMixin:
             return arrow.get(self.created_at.isoformat(), tzinfo=tzinfo).format(format, locale=locale)
         return ' --- '
 
-    @property
     def update_date(self, tzinfo='Europe/Istanbul', locale='tr_tr', format='DD MMMM YYYY dddd HH:mm'):
         if self.updated_at is not None:
             timedelta = round((datetime.now() - self.created_at).days)
